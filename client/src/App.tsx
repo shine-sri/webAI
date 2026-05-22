6624b6f55920e1a72d8fa0d0112401af4dfe0c9d
Shine Srivastava <shinesri.code@gmail.com>
2026-05-23 02:06:54 +0530

    Clean project upload

---
+++ client/src/App.tsx
@@ -0,0 +1,56 @@
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Pricing from './pages/Pricing'
+import Projects from './pages/Projects'
+import MyProjects from './pages/MyProjects'
+import Preview from './pages/Preview'
+import Community from './pages/Community'
+import View from './pages/View'
+import Navbar from './components/Navbar'
+import { Toaster } from 'sonner'
+import AuthPage from './pages/auth/AuthPage'
+import Settings from './pages/Settings'
+import Loading from './pages/Loading'
+
+const App = () => {
+
+  const { pathname } = useLocation()
+
+  const hideNavbar = pathname.startsWith('/projects/') && pathname !== '/projects'
+                      || pathname.startsWith('/view/')
+                      || pathname.startsWith('/preview/')
+
+  return (
+    // Added Tailwind classes here to ensure the footer is pushed to the bottom of the page
+    <div className="min-h-screen flex flex-col">
+    <Toaster />
+      {!hideNavbar && <Navbar />}
+      
+      {/* Added flex-grow to ensure the main content takes up available space */}
+      <main className="flex-grow">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/pricing' element={<Pricing />} />
+          <Route path='/projects/:projectId' element={<Projects />} />
+          <Route path='/projects' element={<MyProjects />} />
+          <Route path='/preview/:projectId' element={<Preview />} />
+          <Route path='/preview/:projectId/:versionId' element={<Preview />} />
+          <Route path='/community' element={<Community />} />
+          <Route path='/view/:projectId' element={<View />} />
+          <Route path="/auth/:pathname" element={<AuthPage />} />
+          <Route path="/account/settings" element={<Settings />} />
+          <Route path='/loading' element={<Loading />}/>
+        </Routes>
+      </main>
+
+      {/* Added the Footer exactly here, hiding it on the same pages as the Navbar */}
+      {!hideNavbar && (
+        <footer className="w-full text-center p-4 mt-auto text-gray-500 text-sm">
+          Created by Shine Srivastava
+        </footer>
+      )}
+    </div>
+  )
+}
+
+export default App
